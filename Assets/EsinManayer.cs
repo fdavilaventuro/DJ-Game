119155937c00b08ade20701f0e1ccfcf4f0d26fd
fdavilaventuro <fabio.davila@utec.edu.pe>
2025-12-05 18:17:09 -0500

    feat: scene switching

---
+++ Assets/EsinManayer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.Events;
+
+
+public class EsinManayer : MonoBehaviour
+{
+[SerializeField] Animator FadePanelAnimator;
+public UnityEvent whenSceneChanged;
+
+public void LoadEnv(int sceneIdx)
+{
+whenSceneChanged?.Invoke();
+StartCoroutine(Mondongo(sceneIdx));
+}
+
+IEnumerator Mondongo(int sceneIdx)
+{
+AsyncOperation syn = SceneManager.LoadSceneAsync(sceneIdx);
+FadePanelAnimator.SetTrigger("fade");
+while (!syn.isDone)
+	yield return null;
+}
+
+}
